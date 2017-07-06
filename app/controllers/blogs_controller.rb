9b70a73eeb76ad9291933a4817c315a5af113a91
Jordan Hudgens <jordan@devcamp.com>
2017-07-06 17:19:31 +0000

    draft publish

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,6 @@
 class BlogsController < ApplicationController
-  before_action :set_blog, only: [:show, :edit, :update, :destroy]
+  before_action :set_blog, only: [:show, :edit, :update, :destroy, :toggle_status
+  ]
 
   # GET /blogs
   # GET /blogs.json
@@ -61,6 +62,19 @@ class BlogsController < ApplicationController
     end
   end
 
+ def toggle_status
+ if @blog.draft?
+   @blog.published!
+   elsif @blog.published?
+   @blog.draft!
+ end 
+
+ redirect_to blogs_url, notice: "post status updated"
+ end
+
+  
+
+ 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_blog
@@ -72,3 +86,5 @@ class BlogsController < ApplicationController
       params.require(:blog).permit(:title, :body)
     end
 end
+ 
+ 
