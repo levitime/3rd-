e2c2691e5aae73a8d1adca2f262a99a02a3dc187
Jordan Hudgens <jordan@devcamp.com>
2017-07-12 14:38:08 +0000

    deafults

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,4 +6,12 @@ validates_presence_of :title, :body, :main_image, :thumb_image
   end
 
   scope :ruby_on_rails_portfolio_items, -> {  where(subtitle: "Ruby on Rails")}
+
+  after_initialize :set_defaults 
+
+def set_defaults
+  self.main_image ||= "http://placehold.it/600x200" 
+    self.thumb_image ||= "http://placehold.it/350x150"
+end 
+
 end 
