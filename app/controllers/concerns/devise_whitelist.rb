1d4259aa29b0651edee4fd1e9754e4771a816b3f
Jordan Hudgens <jordan@devcamp.com>
2017-07-18 15:53:58 +0000

    devise whitelist

---
+++ app/controllers/concerns/devise_whitelist.rb
@@ -0,0 +1,13 @@
+module DeviseWhitelist
+  extend ActiveSupport::Concern
+
+  included do
+    before_filter :config_permitted_parameters, if: :devise_controller?
+  end
+
+def config_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+        devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+end
+
+end
